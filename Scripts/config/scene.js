4ad81a6c0b0e044f769a208f546d6cb634a7fea0
Francis Acheampong <francisacheampong@gmail.com>
2018-03-17 06:22:46 -0400

    alpha build

---
+++ Scripts/config/scene.js
@@ -0,0 +1,12 @@
+var config;
+(function (config) {
+    var Scene;
+    (function (Scene) {
+        Scene[Scene["START"] = 0] = "START";
+        Scene[Scene["PLAY1"] = 1] = "PLAY1";
+        Scene[Scene["PLAY2"] = 2] = "PLAY2";
+        Scene[Scene["PLAY3"] = 3] = "PLAY3";
+        Scene[Scene["OVER"] = 4] = "OVER";
+    })(Scene = config.Scene || (config.Scene = {}));
+})(config || (config = {}));
+//# sourceMappingURL=scene.js.map
---
+++ Scripts/config/scene.ts
@@ -0,0 +1,9 @@
+module config {
+  export enum Scene {
+    START,
+    PLAY1,
+    PLAY2,
+    PLAY3,
+    OVER
+  }
+}
